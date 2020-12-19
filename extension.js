19043753977baf9587e81e8e7082d30eb14c9247
Koustav Pal <koustav.pal@bluecoppertech.com>
2020-12-19 13:32:03 +0530

    initial commit

---
+++ extension.js
@@ -0,0 +1,120 @@
+// The module 'vscode' contains the VS Code extensibility API
+// Import the module and reference it with the alias vscode in your code below
+const vscode = require('vscode');
+const axios = require('axios').default;
+
+// this method is called when your extension is activated
+// your extension is activated the very first time the command is executed
+
+/**
+ * @param {vscode.ExtensionContext} context
+ */
+function activate(context) {
+
+	console.log('Congratulations, your extension "test-extension" is now active!');
+
+	let disposable = vscode.commands.registerCommand('test-extension.helloWorld', async () => {
+		// The code you place here will be executed every time your command is executed
+
+		// Display a message box to the user
+		// vscode.window.showInformationMessage('Hello World from the other side!');
+
+		// let date = new Date();
+		// let second = date.getSeconds();
+		// let minutes = date.getMinutes();
+		// let hour = date.getHours();
+
+		// vscode.window.showInformationMessage(`the current time is --> ${hour}:${minutes}:${second}`);
+
+		// let value = await vscode.window.showInputBox();
+		// vscode.window.showInformationMessage(`the current time is --> ${value}`);
+
+		const weapon_options = [
+			'AK-47', 
+			'AUG', 
+			'AWP', 
+			'Bayonet', 
+			'Bowie Knife', 
+			'Butterfly Knife', 
+			'Classic Knife', 
+			'CZ75-Auto', 
+			'Desert Eagle', 
+			'Dual Berettas', 
+			'Falchion Knife', 
+			'FAMAS', 
+			'Five-SeveN', 
+			'Flip Knife', 
+			'G3SG1', 
+			'Galil AR', 
+			'Glock-18', 
+			'Gut Knife', 
+			'Huntsman Knife', 
+			'Karambit', 
+			'M249', 
+			'M4A1-S', 
+			'M4A4', 
+			'M9 Bayonet', 
+			'MAC-10', 
+			'MAG-7', 
+			'MP5-SD', 
+			'MP7', 
+			'MP9', 
+			'Navaja Knife', 
+			'Negev', 
+			'Nomad Knife', 
+			'Nova', 
+			'P2000', 
+			'P250', 
+			'P90', 
+			'Paracord Knife', 
+			'PP-Bizon', 
+			'R8 Revolver', 
+			'Sawed-Off', 
+			'SCAR-20', 
+			'SG 553', 
+			'Shadow Daggers', 
+			'Skeleton Knife', 
+			'SSG 08', 
+			'Stiletto Knife', 
+			'Survival Knife', 
+			'Talon Knife', 
+			'Tec-9', 
+			'UMP-45', 
+			'Ursus Knife', 
+			'USP-S', 
+			'XM1014'
+		];
+		
+		let quickPick = await vscode.window.showQuickPick(weapon_options).then(response => {
+			console.log(response);
+			return response;
+		});
+
+		let name = await vscode.window.showInputBox({
+			placeHolder: "Name"
+		})
+
+		let wear = await vscode.window.showInputBox({
+			placeHolder: "Wear"
+		})
+
+		vscode.window.showInformationMessage(`quickPick --> ${quickPick}`);
+		vscode.window.showInformationMessage(`name --> ${name}`);
+		vscode.window.showInformationMessage(`wear --> ${wear}`);
+		
+
+	});
+
+	context.subscriptions.push(disposable);
+}
+// @ts-ignore
+exports.activate = activate;
+
+// this method is called when your extension is deactivated
+function deactivate() {}
+
+module.exports = {
+	// @ts-ignore
+	activate,
+	deactivate
+}
